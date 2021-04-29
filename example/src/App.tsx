f62dbfd946d695e53c7ec5298ca7c2bb2ee06bac
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-04-29 11:33:00 +0300

    add example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AceEditor from "react-ace";
+
+import "ace-mode-wiki";
+import "ace-builds/src-noconflict/theme-github";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AceEditor
+      mode="wiki"
+      // theme="github"
+      //name="example"
+      editorProps={{ $blockScrolling: true }}
+    />
   );
 }
 
